6215a5376c6d17b426ddea61ac4b81b25fa4eb78
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-18 19:05:37 -0300

    init Xylophone App

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Placeholder(),
+        ),
+      ),
+    );
+  }
+}
