91175442a03bce0492c3b7c7d119d21f469a0533
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-18 17:03:01 -0300

    add image asset

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,7 +18,42 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.orange.shade900,
           title: const Text('Dice'),
         ),
+        body: const DicePage(),
       ),
     );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+      children: [
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              print('left');
+            },
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              print('right');
+            },
+            child: Image.asset('images/dice2.png'),
+          ),
+        ),
+      ],
+    ));
   }
 }
