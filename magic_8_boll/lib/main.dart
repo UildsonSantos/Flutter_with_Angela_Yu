4fcd3aeda4014efae0bdfe9d399b428bf5574fb3
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-18 18:17:33 -0300

    init MAgic 8 Boll App

---
+++ magic_8_boll/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: const Center(
+            child: Text(
+              'Ask Me Anything',
+              style: TextStyle(fontSize: 37.0),
+            ),
+          ),
+        ),
+        body: const Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
