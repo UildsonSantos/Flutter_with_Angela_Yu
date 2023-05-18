a7fc53e246f2e777b319fcbfa2495647e3df08e0
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-18 16:49:14 -0300

    init App Dicee

---
+++ dicee/android/app/src/main/java/dos/santos/uildson/dicee/MainActivity.java
@@ -0,0 +1,6 @@
+package dos.santos.uildson.dicee;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ dicee/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.orange.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.orange.shade900,
+          title: const Text('Dice'),
+        ),
+      ),
+    );
+  }
+}
