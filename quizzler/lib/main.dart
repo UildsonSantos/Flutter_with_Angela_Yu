7f15406a5da600a21d89aee847cb51d3ed04ab0d
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-19 00:43:43 -0300

    basic layout

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -13,7 +13,12 @@ class Quizzler extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        body: const QuizPage(),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
     );
   }
@@ -29,6 +34,55 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.green,
+            child: const Text(
+              'True',
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+            ),
+            onPressed: () {
+              //The user picked true.
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.red,
+            child: const Text(
+              'False',
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+            ),
+            onPressed: () {
+              //The user picked false.
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
