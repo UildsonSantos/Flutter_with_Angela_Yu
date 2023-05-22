6ee237263e8d203912088f1a207eb6144990599a
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-05-21 21:35:34 -0300

    implement layout designer

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const double bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -15,26 +19,83 @@ class _InputPageState extends State<InputPage> {
         title: const Center(child: Text('BMI CALCULATOR')),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: const [
-                Expanded(child: Placeholder()),
-                Expanded(child: Placeholder()),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Placeholder(),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Placeholder(),
+                  ),
+                ),
               ],
             ),
           ),
-          const Expanded(child: Placeholder()),
+          const Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+              cardChild: Placeholder(),
+            ),
+          ),
           Expanded(
             child: Row(
               children: const [
-                Expanded(child: Placeholder()),
-                Expanded(child: Placeholder()),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Placeholder(),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Placeholder(),
+                  ),
+                ),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColour,
+            margin: const EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+            width: double.infinity,
+          )
         ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  final Widget cardChild;
+
+  const ReusableCard({
+    Key? key,
+    required this.colour,
+    required this.cardChild
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      height: 200.0,
+      width: 170.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
+      ),
+      child: cardChild,
     );
   }
 }
